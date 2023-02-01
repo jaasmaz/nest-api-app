6e70b84d3ee16827c30cd24d97c823cd60f6b22d
jaasmaz <jaasmaz@gmail.com>
2023-01-31 17:11:58 -0800

    save user to db on signing up

---
+++ prisma/migrations/20230201010947_init/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "firtsName" DROP NOT NULL,
+ALTER COLUMN "lastName" DROP NOT NULL;
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
-    return { message: 'This is Signup route' };
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    // save the user to the database
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    // return the user
+    return user;
   }
 
   signin() {
